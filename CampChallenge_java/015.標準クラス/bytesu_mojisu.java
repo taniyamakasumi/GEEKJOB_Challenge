766ffe6f89c23c69808c5025eea234ed5ee3d78c
taniyamakasumi <kasumi.taniyama@gmail.com>
2018-04-12 11:19:00 +0900

    Java15-5課題提出します

---
+++ CampChallenge_java/015.標準クラス/bytesu_mojisu.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mojiretu;
+
+/**
+ *
+ * @author kasum
+ */
+public class bytesu_mojisu {
+    public static void main(String[] args) {
+    String myName="谷山香澄";
+    System.out.println("バイト数は"+myName.getBytes().length+"です。<br>");
+    System.out.println("文字数は"+myName.length()+"です。");
+    } 
+}
