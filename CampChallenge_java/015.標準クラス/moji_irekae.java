9dc237eabf1b6ed8db5fd1c92f0eb0ccb0e2dce0
taniyamakasumi <kasumi.taniyama@gmail.com>
2018-04-12 12:05:11 +0900

    Java15-7課題提出します

---
+++ CampChallenge_java/015.標準クラス/moji_irekae.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mojiretu;
+
+/**
+ *
+ * @author kasum
+ */
+public class moji_irekae {
+    public static void main(String[] args) {
+    String sentence="きょUはぴIえIちぴIのくみこみかんすUのがくしゅUをしてIます" ; 
+    String sentence1=(sentence.replace("I","い"));
+    System.out.println(sentence1.replace("U","う"));
+    }
+}
