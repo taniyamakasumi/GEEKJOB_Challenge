00d898008bbc585ee2be5c434b156576463d72d4
taniyamakasumi <kasumi.taniyama@gmail.com>
2018-04-12 19:25:07 +0900

    Java15-8課題提出します

---
+++ CampChallenge_java/015.標準クラス/File_kakidasi_hozon.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package File_sosa;
+
+import java.io.*;
+
+/**
+ *
+ * @author kasum
+ */
+public class File_kakidasi_hozon {
+    public static void main(String[] args) throws IOException {
+    try{
+        File fp = new File("test.txt");
+    FileWriter fw = new FileWriter(fp,true);
+    fw.write("谷山香澄です。<br>福岡県出身です。<br>よろしくお願いします。");
+    fw.close();
+    }catch(IOException e){
+        System.out.println(e);}
+    }
+    
+}
